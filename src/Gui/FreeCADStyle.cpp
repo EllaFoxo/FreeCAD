6acbd2a6614c5235911507061531ad9abf1cc2ed
Kacper Donat <kadet1090@gmail.com>
2026-03-18 00:22:24 +0100

    Gui: Fix QComboBox expanding beyond available layout space
    Switch the size-adjust policy to AdjustToMinimumContentsLengthWithIcon
    in polish(). The default AdjustToContents policy makes sizeHint() grow
    with the longest item, forcing layouts to over-allocate horizontal space.
    With this change, the combo box constrains itself to minimumContentsLength
    (default 0), allowing layouts to clip/elide it freely while still
    rendering the selected item in full.

--- src/Gui/FreeCADStyle.cpp
+++ src/Gui/FreeCADStyle.cpp
@@ -1976,6 +1976,10 @@ void FreeCADStyle::polish(QWidget* widget)
             IconManager::instance().icon(":/icons/bound-expression-symbol.svg")
         );
     }
+
+    if (auto* comboBox = qobject_cast<QComboBox*>(widget)) {
+        comboBox->setSizeAdjustPolicy(QComboBox::AdjustToMinimumContentsLengthWithIcon);
+    }
 }
 
 void FreeCADStyle::unpolish(QWidget* widget)
