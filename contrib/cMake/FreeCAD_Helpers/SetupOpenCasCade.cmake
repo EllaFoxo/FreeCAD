c559eb50d04cd05e02c1efcb78596219cd18d829
Kacper Donat <kadet1090@gmail.com>
2026-08-07 10:46:56 +0200

    fixup! Gui: Adjust list spacing

--- contrib/cMake/FreeCAD_Helpers/SetupOpenCasCade.cmake
+++
@@ -1,8 +0,0 @@
-macro(SetupOpenCasCade)
-    find_package(OCC)
-    if(NOT OCC_FOUND)
-        message(FATAL_ERROR "================================================================\n"
-                            "OpenCASCADE not found!\n"
-                            "================================================================\n")
-    endif()
-endmacro()
--- contrib/cMake/FreeCAD_Helpers/SetupPybind11.cmake
+++
@@ -1,9 +0,0 @@
-macro(SetupPybind11)
-# -------------------------------- PyBind11 -----------------------------
-
-    # necessary for flat-mesh feature
-    if (FREECAD_USE_PYBIND11)
-        find_package(pybind11 REQUIRED)
-    endif()
-
-endmacro(SetupPybind11)
